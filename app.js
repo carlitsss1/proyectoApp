c27fc579b0ed6c2ea2eef9ddfaa884c1cc6bbfc1
carlitsss1 <cuentadecarlitos1@gmail.com>
2025-11-19 02:45:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/uploads', express.static('uploads'));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/productos', productosRoutes);
 app.use('/api/pedidos', pedidosRoutes);
@@ -32,10 +33,12 @@ app.use('/api/cuentas', cuentasRoutes);
 app.use('/api/pagos', pagosRoutes);
 app.use('/api/reportes', reportesRoutes);
 app.use('/api/usuarios', usuariosRoutes);
+
 app.use(express.static(path.join(__dirname, 'www')));
-app.get('*', (req, res) => {
+
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
+// -----------------------------------------
 
 export default app;
-
