e8d5ca4ef9695897ecfe3e831c9b86ccffab9dfd
carlitsss1 <cuentadecarlitos1@gmail.com>
2025-11-19 03:06:23 -0500

    Update mailer.js

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,11 +1,29 @@
-import nodemailer from "nodemailer";
+import Brevo from "sib-api-v3-sdk";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "9bf69c001@smtp-brevo.com",
-    pass: process.env.BREVO_SMTP_KEY,
-  },
-});
+const client = Brevo.ApiClient.instance;
+client.authentications["api-key"].apiKey = process.env.BREVO_KEY;
+
+export const enviarCorreo = async (destino, codigo) => {
+  try {
+    const api = new Brevo.TransactionalEmailsApi();
+
+    await api.sendTransacEmail({
+      sender: {
+        email: "cuentadecarlitos1@gmail.com",
+        name: "ProyectoApp"
+      },
+      to: [{ email: destino }],
+      subject: "Código de recuperación",
+      htmlContent: `
+        <h2>Recuperación de contraseña</h2>
+        <p>Tu contraseña temporal es:</p>
+        <h1 style="letter-spacing:4px;">${codigo}</h1>
+        <p>Ingresa a la app y cámbiala desde tu perfil.</p>
+      `
+    });
+
+    console.log("Correo enviado correctamente");
+  } catch (error) {
+    console.error("Error al enviar correo Brevo:", error);
+  }
+};
